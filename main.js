df432d114a5e2dcdf1d9e0b87b988ca879800b2d
causadev <causadev@proton.me>
2024-05-09 02:25:23 -0500

    Second Commit!!

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 import './style.css'
+const topInput = document.querySelector("[data-top-text]")
+const bottomInput = document.querySelector("[data-bottom-text]")
+const topText = document.querySelector(".topText")
+const bottomText = document.querySelector(".bottomText")
+
+topInput.addEventListener("input", top)
+
+bottomInput.addEventListener("input", bottom)
+
+
+function top(e) {
+  topText.textContent = e.target.textContent
+}
+
+function bottom(e) {
+  bottomText.textContent = e.target.textContent
+}
 
 
 console.log("Hola!")
